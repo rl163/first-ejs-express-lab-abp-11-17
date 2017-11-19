d86ad68cb419d7d45ae40a790a9346e875f5fb84
Ryan Lee <ryanlee91@gmail.com>
2017-11-19 19:21:08 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+var express = require('express')
+var app = express()
+
+// Build Your Route Here
+app.get("/", function(req, resp){
+  const welcomeText = "Welcome to HTML"
+  const favoriteThings = ["Code","People","Music"]
+  const viewData = {
+    "welcomeText": welcomeText,
+    name:"Avi",
+    colors: ["Red","Yellow","Blue"],
+    "favoriteThings": favoriteThings
+  }
+  resp.render("index", viewData)
+  })
+app.get("/about",function(req,resp){
+  resp.render("about")
+})
+
+
+module.exports = app
 
 
 module.exports = app
